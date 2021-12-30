6836fd2ea3ef802f714747aff8f83a3493808701
newPacAttack <91351390+newPacAttack@users.noreply.github.com>
2021-12-29 17:26:25 -0700

    Add files via upload

---
+++ Plantilla-Proyecto-C37-main/js/Quiz.js
@@ -0,0 +1,61 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //escribe aquí el código para ocultar los elementos de la pregunta
+    question.hide();
+    //escribe aquí el código para cambiar el color de fondo 
+
+    //escribe el código para mostrar un encabezado que indique el resultado del Cuestionario
+    
+    //llama aquí a getContestantInfo( )
+    Contestant.getContestantInfo();
+
+    //escribe la condición para comprobar si contestantInfor no está indefinido 
+    if(allContestants !== undefined){
+      fill("Blue");
+      textSize(20);
+      text("*NOTA: ¡El concursante que respondió correctamente, está resaltado en color verde!", 130, 230);
+    }
+    //escribe aquí el código para agregar una nota
+
+    //escribe el código para resaltar al concursante que respondió correctamente
+    for (var plr in allContestants){
+      var correctAns = "2";
+      if (correctAns === allContestants[plr.answer]){
+        fill("Green")
+      }
+      else{
+        fill("red")
+      }
+    }
+  }
+
+}
